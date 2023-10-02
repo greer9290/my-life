1e4592b9c8c97c99a3e5cd1d703aa18770544e0f
greer92 <greergit@proton.me>
2023-10-01 19:32:46 -0500

    Grid styling, javascript nav menu applied to all pages, reformatting needed

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("barSidenav").style.width = "100%";
+    document.getElementById("nav-img").style.background = black;
+    document.getElementsByClassName("nav-img").style.color = white;
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("barSidenav").style.width = "0";
+}
