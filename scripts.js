791dc724de0ec87c83d9600edf2463d70847eed8
greer92 <greergit@proton.me>
2023-10-03 05:28:27 -0500

    Fix sidenav, responsive navbar roughed in

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function openNav() {
     document.getElementById("barSidenav").style.width = "100%";
     document.getElementById("nav-img").style.background = black;
     document.getElementsByClassName("nav-img").style.color = white;
+    document.getElementByClassName("sidenav").style.right = "-100px";
 }
 
 /* Set the width of the side navigation to 0 */
